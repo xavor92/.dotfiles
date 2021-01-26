9fb0d72cab736f9de95ca4032c2eeff98cbc62e5
Oliver Westermann <owestermann@googlemail.com>
2021-01-26 20:01:17 +0100

    Initial commit adding .tmux config

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# LINKS is an associative list containing a set of src->dest elements for which
+# symlinks will be created. Base dir for src is ~, for dest it's the location
+# of this script
+LINKS=(".tmux.conf.local->.tmux.conf.local"
+".tmux.conf->.tmux/.tmux.conf")
+
+readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+readonly HOMEPATH=~
+
+echo "Linking from ${HOMEPATH} to ${SCRIPTPATH}"
+
+for ((i = 0; i < ${#LINKS[@]}; i++))
+do
+    str="${LINKS[$i]}"
+    src=${str%%->*}
+    dst=${str##*->}
+    ln -vs ${SCRIPTPATH}/${dst} ${HOMEPATH}/${src}
+done
