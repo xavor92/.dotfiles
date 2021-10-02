4d9239293c30fb2cc73de14de611ce414bfd4334
Oliver Westermann <owestermann@googlemail.com>
2021-10-02 22:38:31 +0200

    Add work mode, add basic README
    Some files are only relevant for work setups. Do not add them for
    private setups.
    
    Signed-off-by: Oliver Westermann <owestermann@googlemail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# .dotfiles
+
+Collection of local settings to allow quick setup of new environments.
+
+## Usage
+
+    # create symlinks for private usage
+    # ./setup.sh
+    
+    # create symlinks for work usage
+    $ ./setup.sh -w
--- setup.sh
+++ setup.sh
@@ -8,18 +8,33 @@ LINKS=(
     ".tmux.conf->.tmux/.tmux.conf"
     ".bash_profile->.bash_profile"
     ".bash_ssh->.bash_ssh"
+)
+
+LINKS_WORK=(
     ".bash_work->.bash_work"
 )
 
 readonly SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 readonly HOMEPATH=~
 
-echo "Linking from ${HOMEPATH} to ${SCRIPTPATH}"
+function create_symlink() {
+    src=${HOMEPATH}/${str%%->*}
+    dst=${SCRIPTPATH}/${str##*->}
+    echo "Linking from ${src} to ${dst}"
+    ln -vs ${dst} ${src}
+}
 
 for ((i = 0; i < ${#LINKS[@]}; i++))
 do
     str="${LINKS[$i]}"
-    src=${str%%->*}
-    dst=${str##*->}
-    ln -vs ${SCRIPTPATH}/${dst} ${HOMEPATH}/${src}
+    create_symlink ${str}
 done
+
+if [[ $1 = "-w" ]]
+then
+    for ((i = 0; i < ${#LINKS_WORK[@]}; i++))
+    do
+        str="${LINKS_WORK[$i]}"
+        create_symlink ${str}
+    done
+fi
